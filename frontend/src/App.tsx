b549f75216c99ab570bf862921fad9a5ed1520ff
Darshan Heble <darshanheble@gmail.com>
2024-12-04 00:42:08 +0530

    simplified custom imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import getServerStatus from "./services/getServerStatus";
+import getServerStatus from "@services/getServerStatus";
+// import getServerStatus from "./services/getServerStatus";
 
 function App() {
   const [count, setCount] = useState(0);
