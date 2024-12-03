25397c9368498eae24442a3ee18225d746f072cb
Darshan Heble <darshanheble@gmail.com>
2024-12-03 23:27:02 +0530

    ..

--- frontend/src/services/getServerStatus.ts
+++ frontend/src/services/getServerStatus.ts
@@ -1,8 +1,13 @@
 import api from "./api";
 
-const getServerStatus = async (): Promise<string> => {
-  const response = await api.get("/connection-status");
-  return response.data.status;
+const getServerStatus = async (): Promise<boolean> => {
+  try {
+    const response = await api.get("/connection-status");
+    return response.status === 200;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 };
 
 export default getServerStatus;
