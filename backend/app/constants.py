a5721786e60298c51532a28ed9beaa9dc021d45d
Darshan Heble <darshanheble@gmail.com>
2024-12-06 17:59:09 +0530

    ..

---
+++ backend/app/constants.py
@@ -0,0 +1,4 @@
+BASE_SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
+
+GOOGLE_API_KEY = "AIzaSyBnzK8XGWzZF7YcdrPRSPAicJunxcoIuOg"  # google console api key
+CSE_ID = "9655e6575d1dc4ba5"                                # Custom google search api key
---
+++ backend/app/services/fetchNewsFromGoogle.py
@@ -0,0 +1,34 @@
+#import all libraries and variables
+import requests
+from ..constants import CSE_ID, GOOGLE_API_KEY, BASE_SEARCH_URL
+
+"""
+code explanation here(coming soon)
+
+"""
+def fetchNewsFromGoogle(query):
+    params = {
+        "q": query,
+        "cx": CSE_ID,
+        "key": GOOGLE_API_KEY,
+        "num": 10 # maximum number of articles to to be fetched from Google
+    }
+    
+    response = requests.get(BASE_SEARCH_URL, params=params)
+    response.raise_for_status()     # Raise an error for bad requests
+    
+    return response.json().get("items")
+
+
+
+# example
+# query = "U.S. military to accept transgender recruits on Monday: Pentagon"
+
+# results = google_news_search(query, API_KEY, CSE_ID)
+
+# Print titles and links
+# for result in results:
+#     print(f"Link: {result['displayLink']}\n")
+#     print(f"Title: {result['title']}")
+#     print(f"Link: {result['link']}\n")
+#     print(f"Link: {result['snippet']}\n")
