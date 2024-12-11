07f5c0b251dce2ab91ad107dcf7e74aedac6969e
Darshan Heble <darshanheble@gmail.com>
2024-12-11 21:26:56 +0530

    Added frontend UI part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Header } from "@components/HeaderDifs/Header";
 import "./App.css";
 import getServerStatus from "@services/getServerStatus";
 import verifyNews from "@services/verifyNews";
+import { Body } from "@components/BodyDifs/Body";
 
 function App() {
-  const [count, setCount] = useState(0);
   // get server status
   getServerStatus().then((status) => {
     console.log(status);
@@ -19,26 +17,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Body />
     </>
   );
 }
