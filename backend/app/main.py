c272ca576fa249cc3333451b9cd4cc26eee43c43
Darshan Heble <darshanheble@gmail.com>
2025-04-01 17:31:55 +0530

    Fixed new search using serper.dev

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from .Types.types import InputNewsType, OutputNewsType
 from .services.webScrap import extract_news_from_meta
-from .services.fetchNewsFromGoogle import fetch_news_from_google
+from .services.serper import fetch_news_from_serper
 from .process import m_main
 from dotenv import load_dotenv
 import os
@@ -30,22 +30,23 @@ async def verify_news(news: InputNewsType):
     try:
         category = news.category
         content = news.content
+        url_extracted_news = ""
         
         # ----------------------------------
-        # if category == "url":
-        #     try:
-        #         fetchedNews = extract_news_from_meta(content)
-        #         if not fetchedNews or not fetchedNews.title:
-        #             raise ValueError("Failed to extract title from URL.")
-        #         content = fetchedNews.title
-        #         print("Input URL extracted", content)
-        #     except Exception as e:
-        #         print(f"Error extracting content from URL: {e}")
-        #         raise HTTPException(status_code=400, detail="Invalid or inaccessible URL.")
+        if category == "url":
+            try:
+                fetchedNews = extract_news_from_meta(content)
+                if not fetchedNews or not fetchedNews.title:
+                    raise ValueError("Failed to extract title from URL.")
+                url_extracted_news = fetchedNews.title
+                print("Input URL extracted", url_extracted_news)
+            except Exception as e:
+                print(f"Error extracting content from URL: {e}")
+                raise HTTPException(status_code=400, detail="Invalid or inaccessible URL.")
         # ----------------------------------
         
         result = await m_main(content)
-        relatedNews = await fetch_news_from_google(content)
+        relatedNews = await fetch_news_from_serper(url_extracted_news)
 
         # Return the result (fake or real) and some related news
         return OutputNewsType(label=result, relatedNews=relatedNews)
